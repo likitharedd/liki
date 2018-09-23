be1f05477d7dc54925ec12bfe08e95e034c6e9e8
Likitha Reddy <Likitha Reddy@DESKTOP-6RGVLQS>
2018-09-24 00:53:44 +0530

    commit completed by likitha reddy

---
+++ projectbackend/src/main/java/com/niit/projectbackend/App.java
@@ -0,0 +1,37 @@
+package com.niit.projectbackend;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.configuration.DBConfiguration;
+import com.niit.dao.ProductDao;
+import com.niit.dao.ProductDaoImpl;
+import com.niit.models.Product;
+
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 //TESTING SAVEPRODUCT method in productdaoimpl
+    	//USE JUNIT testing tool to test the method
+        System.out.println( "Hello World!" );
+        //SessionFactory, DataSource,TransactionManager, ProductDaoImpl
+        ApplicationContext context=new AnnotationConfigApplicationContext(DBConfiguration.class,ProductDaoImpl.class);
+       
+        
+        ProductDao productDao=(ProductDao)context.getBean("productDaoImpl");
+        
+        Product product=new Product(); //New Product
+        product.setProductname("power banl"); //inserted in the column productname
+        product.setProductdesc("1nd Edition");//column productdesc
+        product.setPrice(5000); //column price
+        product.setQuantity(9); //column quantity
+        //column id =0
+       
+        
+        productDao.saveProduct(product);//to insert product details into a product table
+        
+    }
+}
